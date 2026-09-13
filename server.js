2d49eeae0cf5139a8aa22dc27e54c1c7a7781e7f
hothiptv <hothiptvnek@gmail.com>
2026-09-13 12:30:11 +0700

    Update server.js

--- server.js
+++ server.js
@@ -12,32 +12,25 @@ const io = new Server(server, {
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Quản lý các tiến trình Python theo Socket ID
 const processes = new Map();
 
 io.on('connection', (socket) => {
-    // 1. Nhận lệnh chạy code từ giao diện
     socket.on('run_code', (code) => {
-        // Nếu có tiến trình cũ đang chạy, hãy diệt nó trước
         if (processes.has(socket.id)) {
             processes.get(socket.id).kill();
         }
 
-        // Chạy Python ở chế độ Unbuffered (-u) để đẩy output realtime
         const pythonProcess = spawn('python3', ['-u', '-c', code]);
         processes.set(socket.id, pythonProcess);
 
-        // Đẩy luồng dữ liệu xuất (stdout) về Frontend
         pythonProcess.stdout.on('data', (data) => {
             socket.emit('output', { type: 'stdout', data: data.toString('utf-8') });
         });
 
-        // Đẩy luồng báo lỗi (stderr) về Frontend
         pythonProcess.stderr.on('data', (data) => {
             socket.emit('output', { type: 'stderr', data: data.toString('utf-8') });
         });
 
-        // Khi chương trình kết thúc
         pythonProcess.on('close', (code) => {
             processes.delete(socket.id);
             socket.emit('process_exit', { code });
@@ -50,7 +43,6 @@ io.on('connection', (socket) => {
         });
     });
 
-    // 2. Nhận dữ liệu nhập từ giao diện (Input) và truyền vào STDIN của Python
     socket.on('input_data', (data) => {
         const pythonProcess = processes.get(socket.id);
         if (pythonProcess && pythonProcess.stdin.writable) {
@@ -58,7 +50,6 @@ io.on('connection', (socket) => {
         }
     });
 
-    // Ngắt tiến trình khi đóng kết nối
     socket.on('disconnect', () => {
         if (processes.has(socket.id)) {
             processes.get(socket.id).kill();
